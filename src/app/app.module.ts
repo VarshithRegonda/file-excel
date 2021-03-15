35a516b7b5f5de0d445bd2f9b0b0196db6bb16e1
varshithRegonda <47526096+VarshithRegonda@users.noreply.github.com>
2021-03-15 09:16:01 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { FileUploadComponent } from './file-upload/file-upload.component';
 import { UploadComponent } from './upload/upload.component';
 import { ReadFileComponent } from './read-file/read-file.component';
 
